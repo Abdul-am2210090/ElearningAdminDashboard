e355a5ba281f4963d71ee1d41be3a0c5c4033e5c
Abdul Rahim Alsheikh Mahmoud <am2210090@qu.edu.qa>
2025-07-27 02:23:56 +0300

    finalcode

--- elearning-dashboard/app/components/Topbar.jsx
+++ elearning-dashboard/app/components/Topbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDoorOpen } from '@fortawesome/free-solid-svg-icons';
-export default function Topbar() {
+import { faDoorOpen,faBars } from '@fortawesome/free-solid-svg-icons';
+export default function Topbar({ toggleSidebar }) {
   return (
     <div id="topbar">
+      <FontAwesomeIcon icon={faBars} onClick={toggleSidebar} id='navBTN'/>
       <p>Welcome username</p>
       <button id="logoutBTN"><FontAwesomeIcon icon={faDoorOpen}/>   Logout</button>
     </div>
